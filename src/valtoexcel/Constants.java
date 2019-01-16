04ec564b647b6b7c500aa732e3237f946cc2daea
ricardorusso <russoricardo1@gmail.com>
2019-01-16 22:53:46 +0000

    First

--- src/valtoexcel/Constants.java
+++ src/valtoexcel/Constants.java
@@ -4,4 +4,6 @@ public  abstract class Constants {
 	public static final String QUERY1 = "SELECT * FROM  jobs";
 	public static final String QUERY2 = "SELECT * FROM  employees";
 	private Constants() {}
+	
+	
 }
